9599967022564576cf5183b853bf1fb2388f6bc9
F1rell <ratmir777mustafin@mail.ru>
2022-03-27 16:52:56 +0700

    Тест доработан | добавлена проверка успешности отправки заявки

--- src/test/java/SelenidTest.java
+++ src/test/java/SelenidTest.java
@@ -1,24 +1,22 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import java.time.Duration;
 
-import static com.codeborne.selenide.Condition.visible;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenidTest {
 
-    private WebDriver driver;
-
-    @BeforeAll
-    static void setUpAll() {
-        WebDriverManager.chromedriver().setup();
+    String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
+
+    String planningDate = generateDate(3);
 
     @BeforeEach
     void setupTest() {
@@ -27,21 +25,15 @@ public class SelenidTest {
         $x("//span[@data-test-id='date']//input[@type='tel']").doubleClick().sendKeys(Keys.chord(Keys.CONTROL, "A"), Keys.DELETE);
     }
 
-    @AfterEach
-    void teardown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
-
     @Test
     public void allFieldsAndCheckboxFilledUp() {
         $x("//span[@data-test-id='city']//input[@type='text']").val("Томск");
-        $x("//span[@data-test-id='date']//input[@type='tel']").val("30.03.2022");
+        $x("//span[@data-test-id='date']//input[@type='tel']").val(planningDate);
         $x("//span[@data-test-id='name']//input[@name='name']").val("Иванов Иван");
         $x("//span[@data-test-id='phone']//input[@name='phone']").val("+79520000000");
         $x("//label[@data-test-id='agreement']").click();
         $x("//span[@class='button__text']").click();
-        $x("//*[contains(text(), 'Успешно')]").should(visible, Duration.ofSeconds(15));
+        $x("//div[@class='notification__title']").shouldHave(Condition.text("Успешно!"), Duration.ofSeconds(15));
+        $x("//div[@class='notification__content']").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15));
     }
 }
